a39ff4f5bdcc06747bbbc52af4e7f4019a6e0d93
IllegalTurtle <jkyyen@gmail.com>
2022-05-16 03:28:17 +0530

    Commit

--- js/index.js
+++ js/index.js
@@ -4,15 +4,19 @@ const vh  =(v) => { var h = Math.max(document.documentElement.clientHeight, wind
     return (v * h) / 100;}
 
 var myLazyDiv = document.getElementsByClassName('container')[0];
+var myLazyDiv2 = document.getElementsByClassName('container')[1];
 
 var myScrollFunc = ()=> {
    
     var y = window.scrollY;
-    if (y >= myLazyDiv.offsetHeight-10) {
-      myLazyDiv.className = "container show"
+    if (y >= myLazyDiv.offsetHeight-10 || y >= myLazyDiv2.offsetHeight-10) {
+      myLazyDiv.className = "container show";
+      myLazyDiv2.className = "container show";
+
     } else {
-      myLazyDiv.className = "container hide"
+      myLazyDiv.className = "container hide";
+      myLazyDiv2.className = "container hide";
     }
   };
-  
+
 window.addEventListener("scroll", myScrollFunc);
